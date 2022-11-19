7601f469d308d17fc05d25990558c650543e9f95
Colin Fahrion <colin.fahrion@gmail.com>
2022-11-19 14:12:44 -0800

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: "eslint:recommended",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	rules: {
+		"no-console": "warn",
+	},
+};
