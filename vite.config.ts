691cce112545b366344b684ea53c2b1ba17cb62e
Colin Fahrion <colin.fahrion@gmail.com>
2022-11-21 21:50:30 -0800

    build: revamp to create docs folder and simple dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,32 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 import minifyHTML from "rollup-plugin-minify-html-literals";
+import copy from "rollup-plugin-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	build: {
+		sourcemap: true,
 		rollupOptions: {
-			plugins: [
-				minifyHTML.default(), // instead of minifyHTML(),
-			],
 			input: {
 				index: resolve(__dirname, "index.html"),
 				"theme-3way-switch": resolve(__dirname, "./src/main.ts"),
 			},
-			output: {
-				entryFileNames: "[name].js",
-			},
+			output: [
+				// {
+				// 	entryFileNames: "[name].js",
+				// },
+				{
+					entryFileNames: "[name].js",
+					dir: "docs",
+				},
+			],
+			plugins: [
+				minifyHTML.default(), // instead of minifyHTML(),
+				copy({
+					targets: [{ src: "docs/theme-3way-switch.*", dest: "dist" }],
+				}),
+			],
 		},
 	},
 });
